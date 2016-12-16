3f06f090a0d5999c3d2e9033009ef2bf70b3ee58
Yoko <proton-yoko@users.noreply.github.com>
2016-12-16 15:00:58 +0900

    Update template.c

--- template.c
+++ template.c
@@ -3,6 +3,8 @@
 #include <sys/resource.h>
 
 int main_prg(int, char**);
+void fill_a(char* ans);
+
 
 int main(int argc, char** argv){
 
@@ -23,9 +25,36 @@ int main(int argc, char** argv){
 }
 
 int main_prg(int argc, char** argv){
-
   /** implement here  **/
-  
+    
+    //input mushikui
+    char ans[500001];
+    //strncpy(ans,argv[1],500000);
+    scanf("%s",ans);
+    
+    
+    
+    //x->a
+    fill_a(ans);
+    
+    //input keywords
+    char key[50000];
+    while(scanf("%s",key)==1){
+        //printf("_%s\n",key);
+    }
+    
+    printf("%s\n",ans);
+    
+
   return(0);
 }
 
+void fill_a(char* ans){
+    //x->a
+    int i;
+    for(i=0;i<500001;i++){
+        if(ans[i]=='x'){
+            ans[i]='a';
+        }
+    }
+}
